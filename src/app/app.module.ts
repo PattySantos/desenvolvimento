2e39b44f99376b6296b87b36f5c0c3ebfa38ebfc
patricia <patriciadeo.santos@gmail.com>
2020-06-04 16:16:55 -0300

    login redirecionando pra pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import {
    
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
+    
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component } from '@angular/core';
+import { NbLoginComponent } from '@nebular/auth';
+
+@Component({
+  selector: 'ngx-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent extends NbLoginComponent {
+
+  username:string
+  password:string;
+  loginError: boolean;
+  onSubmit(){
+    console.log(`User: ${this.username} , Pass: ${this.password}`)
+  }
+}
