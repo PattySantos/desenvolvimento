a009892bf51ed8e4ff79812cbc122121f26b1934
patricia <patriciadeo.santos@gmail.com>
2020-06-04 23:22:11 -0300

    login e cadastro de usuário com logo

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -5,17 +5,34 @@
  */
 import { Component } from '@angular/core';
 import { NbLoginComponent } from '@nebular/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent extends NbLoginComponent {
+export class LoginComponent {
 
   username:string
   password:string;
   loginError: boolean;
+  cadastrando:boolean;
+  
+  constructor(
+    private route: Router
+  ){
+
+  }
   onSubmit(){
-    console.log(`User: ${this.username} , Pass: ${this.password}`)
+   this.route.navigate(['/pages'])
+  }
+
+  preparaCadastrar(event){
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelarCadastrar(){
+    this.cadastrando = false;
   }
 }
